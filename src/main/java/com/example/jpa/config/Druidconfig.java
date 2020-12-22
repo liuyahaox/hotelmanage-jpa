6f2c0afbb9ab6d593ab08954fa4945673f2a0e81
liuyahao <545604426@qq.com>
2020-12-22 20:39:15 +0800

    2

--- src/main/java/com/example/jpa/config/Druidconfig.java
+++ src/main/java/com/example/jpa/config/Druidconfig.java
@@ -8,11 +8,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
 import com.alibaba.druid.support.http.WebStatFilter;
-
+@Configuration
 public class Druidconfig {
     @Bean
     // 将所有前缀为spring.datasource下的配置项都加载DataSource中

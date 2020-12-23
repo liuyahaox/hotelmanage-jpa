5966f2fc6e9149ecb43fdb1ac659f39fe51294b4
liuyahao <545604426@qq.com>
2020-12-23 18:47:28 +0800

    1.1

--- src/main/java/com/example/jpa/Customer/CustomerService.java
+++ src/main/java/com/example/jpa/Customer/CustomerService.java
@@ -1,68 +1,46 @@
 package com.example.jpa.Customer;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import com.example.jpa.Record.Record;
-import com.example.jpa.Record.RecordDao;
+import com.example.jpa.Record.RecordService;
 import com.example.jpa.Room.Room;
-import com.example.jpa.Room.RoomDao;
+import com.example.jpa.Room.RoomService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class CustomerService {
     @Autowired
     CustomerDao customerdao;
-    @Autowired
-    RecordDao recordDao;
-    @Autowired
-    RoomDao roomDao;
 
-    public boolean querryifbook(Integer id){
-        Room room = roomDao.findById(id).get();
-        if(room.isIsbooked()){
+    RecordService recordService = new RecordService();
+    RoomService roomService = new RoomService();
+
+    public boolean querryifbook(Integer id) {
+        Room room = roomService.Findroombyid(id);
+        if (room.isIsbooked()) {
             return true;
-        }
-        else return false;
-    }
-    
-
-    public boolean bookroom(Integer id){
-        Room room = new Room();
-        if(querryifbook(id)){
+        } else
             return false;
-        }
-        else 
-            room = roomDao.findById(id).get();
-            room.setIsbooked(true);
-            roomDao.save(room);
-            return true; 
     }
 
-    public boolean checkin(Integer roomid,Integer customerid){
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Room room = roomDao.findById(roomid).get();
-        Record record = new Record();
-        if(querryifbook(roomid)){
+    public boolean bookroom(Integer id) {
+        if (querryifbook(id)) {
+            return false;
+        } else
+            return roomService.bookroom(id);
+    }
+
+    public boolean checkin(Integer roomid, Integer customerid) {
+        if (querryifbook(roomid)) {
             return false;
         }
-        Customer customer = customerdao.findById(customerid).get();
-        room.setIsempty(false);
-        room.setIsbooked(true);
-        customer.setRoomid(roomid);
-        roomDao.save(room);
-        record.setCustomerid(customerid);
-        record.setRoomid(roomid);
-        record.setStime(df.format(new Date()));
-        recordDao.save(record);
+        roomService.checkin(roomid, customerid);
+        recordService.saverecord(customerid, roomid);
+        customerdao.findById(customerid).get().setRoomid(roomid);
         return true;
 
     }
 
-    public void checkout(){
+    public void checkout() {
 
     }
 
-
-    
 }
---
+++ src/main/java/com/example/jpa/Room/RoomService.java
@@ -0,0 +1,28 @@
+package com.example.jpa.Room;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RoomService {
+    @Autowired
+    RoomDao roomDao;
+
+    public Room Findroombyid(Integer id) {
+        return roomDao.findById(id).get();
+    }
+
+    public boolean bookroom(Integer id) {
+
+        Room room = roomDao.findById(id).get();
+        room.setIsbooked(true);
+        roomDao.save(room);
+        return true;
+    }
+
+    public void checkin(Integer roomid, Integer customerid) {
+        Room room = roomDao.findById(roomid).get();
+        room.setIsbooked(true);
+        room.setIsempty(false);
+        roomDao.save(room);
+    }
+
+}
